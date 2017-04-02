7aef4945e11017604c75cf75c38bb49e9dff54ff
IuliaSimion <iulia_20ian@yahoo.com>
2017-04-02 20:59:24 +0300

    PrimeNumbersMPI

---
+++ PrimeNumbers/PrimeNumbers/Prime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    public class Prime
+    {
+       public void IsPrime(int n)
+        {
+            int i, count = 0;
+
+            for (i = 1; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                    count++;
+            }
+
+            if (count == 1)
+                Console.WriteLine(n);
+
+        }
+
+    }
+}
---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MPI;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(new MPI.Environment(ref args))
+            {
+                int n, mystart, stride, rank, ntasks;
+                int LIMIT = 25;
+                Prime prime = new Prime();
+                var comm = Communicator.world;
+                rank = comm.Rank;
+                ntasks = comm.Size;
+
+                mystart = (rank * 2) + 1;
+                stride = ntasks * 2;
+
+                if (rank == 0)
+                {
+                    for (n = mystart; n <= LIMIT; n = n + stride)
+                    {
+                        prime.IsPrime(n);
+
+                    }
+                }
+
+                Console.WriteLine("Finished");
+
+
+                if (rank > 0)
+                {
+                    for (n = mystart; n <= LIMIT; n = n + stride)
+                        prime.IsPrime(n);
+
+                }
+
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
