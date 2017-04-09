12dda2f397809269fc8c43a6c1b93af034ac9d81
IuliaSimion <iulia_20ian@yahoo.com>
2017-04-09 21:11:46 +0300

    RoyFloyd

---
+++ RoyFloyd/RoyFloyd/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MPI;
+
+namespace RoyFloyd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int INF = RoyFloyd.INF;
+            int[,] graph = {
+            { 0,   5,  INF, 10 },
+            { INF, 0,   3, INF },
+            { INF, INF, 0,   1 },
+            { INF, INF, INF, 0 }};
+
+            using (new MPI.Environment(ref args))
+            {
+                int ntasks, rank;
+                Intracommunicator comm = Communicator.world;
+                rank = comm.Rank;
+                ntasks = comm.Size;
+
+                RoyFloyd.FloydWarshall(graph, 4, rank, ntasks, comm);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RoyFloyd/RoyFloyd/RoyFloyd.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MPI;
+
+namespace RoyFloyd
+{
+    public class RoyFloyd
+    {
+        public const int INF = 99999;
+
+        private static void Print(int[,] distance, int verticesCount, int rank)
+        {
+            Console.WriteLine("Shortest distances between every pair of vertices:");
+
+            for (int i = 0; i < verticesCount; ++i)
+            {
+                for (int j = 0; j < verticesCount; ++j)
+                {
+                    if (rank == 0)
+                    {
+                        if (distance[i, j] == INF)
+                            Console.Write("INF".PadLeft(7));
+                        else
+                            Console.Write(distance[i, j].ToString().PadLeft(7));
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public static void FloydWarshall(int[,] graph, int verticesCount, int rank, int ntasks, Intracommunicator comm)
+        {
+            int[,] distance = new int[verticesCount, verticesCount];
+
+
+            for (int i = rank; i < verticesCount; i += ntasks)
+                for (int j = 0; j < verticesCount; ++j)
+                    distance[i, j] = graph[i, j];
+
+            for (int k = 0; k < verticesCount; ++k)
+            {
+                for (int i = rank; i < verticesCount; i += ntasks)
+                {
+                    for (int j = 0; j < verticesCount; ++j)
+                    {
+                        if (distance[i, k] + distance[k, j] < distance[i, j])
+                            distance[i, j] = distance[i, k] + distance[k, j];
+                    }
+                }
+            }
+
+            comm.Gather<int[,]>(distance, 0);
+            Print(distance, verticesCount, rank);
+        }
+    }
+}
